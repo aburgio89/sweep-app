a28815513e39d44f2df8905a397b13dcc170ce00
Anna Burgio <aburgio@wgu.edu>
2026-07-30 11:40:21 -0400

    Added manifest, some gradle edits and SDK change to fix a compatibility issue.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sweepapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.sweepapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1"
     }
@@ -37,19 +37,16 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
-
-    dependencies {
-        implementation("androidx.core:core-ktx:1.19.0")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
-        implementation("androidx.activity:activity-compose:1.13.0")
-        implementation(platform("androidx.compose:compose-bom:2026.06.01"))
-        implementation("androidx.compose.ui:ui")
-        implementation("androidx.compose.ui:ui-graphics")
-        implementation("androidx.compose.ui:ui-tooling-preview")
-        implementation("androidx.compose.material3:material3")
-        implementation("androidx.navigation:navigation-compose:2.9.8")
-        debugImplementation("androidx.compose.ui:ui-tooling")
-
-    }
-
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation(platform("androidx.compose:compose-bom:2026.06.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.9.8")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
